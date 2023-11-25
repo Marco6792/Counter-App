0e3931c66caf6eeda3d1d196a2ba013d95a8a796
Marco6792 <marykang400@gmail.com.com>
2023-11-25 05:35:26 +0100

    rrrrrrrrrrr

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -25,7 +25,7 @@ const Counter = ({ children }: ChildrenType) => {
 			<Input
 				placeholder="Enter a number"
 				onChange={handleChangeInput}
-				className="focus-visible:ring-0 mt-4 z-50 ml-4 py-4"
+				className="focus-visible:ring-0 mt-4 z-50 ml-4 py-4 h-12"
 			/>
 			<div className="flex space-x-10 mt-8 justify-center items-center">
 				<Button onClick={increment} size="lg">
